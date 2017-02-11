c8312e4aa88a4e27ccfe990310f4cf9a380cad03
alessio <alessio.vertemati@gmail.com>
2017-02-11 11:29:56 +0100

    ops, forgot to change provider class names

--- src/MaterialIconsBridgeServiceProvider.php
+++ src/MaterialIconsBridgeServiceProvider.php
@@ -8,7 +8,7 @@ class MaterialIconsBridgeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        app(IconsetFactory::class)->registerBladeTag();
+        app(MaterialIconsBridgeFactory::class)->registerBladeTag();
 
         $this->publishes([
             __DIR__.'/../config/materialiconset.php' => config_path('materialiconset.php'),
@@ -17,12 +17,12 @@ class MaterialIconsBridgeServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(IconsetFactory::class, function () {
+        $this->app->singleton(MaterialIconsBridgeFactory::class, function () {
             $config = [
                 'class' => config('materialiconset.class') ? config('materialiconset.class') : 'icon',
-                'icon_path' => config('materialiconset.icon_path') ? base_path(config('materialiconset.icon_path')) : IconsetFactory::DEFAULT_ICONSET_PATH,
+                'icon_path' => config('materialiconset.icon_path') ? base_path(config('materialiconset.icon_path')) : MaterialIconsBridgeFactory::DEFAULT_ICONSET_PATH,
             ];
-            return new IconsetFactory($config);
+            return new MaterialIconsBridgeFactory($config);
         });
     }
 }
